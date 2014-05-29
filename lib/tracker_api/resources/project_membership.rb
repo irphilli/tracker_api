a4dab0549bec80c8241a5ba05a3ebb453de2ef53
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2014-05-29 13:17:45 +0800

    Adds Project Membership

---
+++ lib/tracker_api/resources/project_membership.rb
@@ -0,0 +1,18 @@
+module TrackerApi
+  module Resources
+    class ProjectMembership
+      include Virtus.model
+
+      attribute :client
+
+      attribute :id, Integer
+      attribute :person_id, Integer
+      attribute :project_id, Integer
+      attribute :role, String
+      attribute :project_color, String
+      attribute :wants_comment_notification_emails, Boolean
+      attribute :kind, String
+      attribute :person, TrackerApi::Resources::Person
+    end
+  end
+end
