c98e36e01f94bfaa8b04230469988f16bbae4ae4
Forest <forestcarlisle@gmail.com>
2014-05-29 21:22:10 -0700

    Updated the exception message.

--- lib/tracker_api/endpoints/memberships.rb
+++ lib/tracker_api/endpoints/memberships.rb
@@ -9,7 +9,7 @@ module TrackerApi
 
       def get(project_id, params={})
         data = client.paginate("/projects/#{project_id}/memberships", params: params)
-        raise TrackerApi::Errors::UnexpectedData, 'Array of stories expected' unless data.is_a? Array
+        raise TrackerApi::Errors::UnexpectedData, 'Array of memberships expected' unless data.is_a? Array
 
         data.map { |membership| Resources::ProjectMembership.new({ client: client }.merge(membership)) }
       end
