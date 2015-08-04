b7fb91a2354d8c2e43b93cf90ffc615cebf4f321
Earle Bunao <earle.bunao@gmail.com>
2015-08-04 11:14:21 +0800

    Add client parameter in get

--- lib/tracker_api/endpoints/epic.rb
+++ lib/tracker_api/endpoints/epic.rb
@@ -10,7 +10,7 @@ module TrackerApi
       def get(project_id, id, params={})
         data = client.get("/projects/#{project_id}/epics/#{id}", params: params).body
 
-        Resources::Epic.new({ project_id: project_id }.merge(data))
+        Resources::Epic.new({ client: client, project_id: project_id }.merge(data))
       end
 
       def create(project_id, params={})
